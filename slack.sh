0115ad9ae7314ead9d53d3c4c372eb44ea522979
CSCI-KNOX <39603273+CSCI-KNOX@users.noreply.github.com>
2018-06-06 16:25:48 -0600

    Add files via upload
    Changed to new CSCI-3753-SUMMER slack channel

--- slack.sh
+++ slack.sh
@@ -10,6 +10,6 @@ echo "\\n\\n" >> /tmp/slack.data.txt
 ifconfig wlan0 >> /tmp/slack.data.txt
 echo \"}    >> /tmp/slack.data.txt
 curl -X POST -H 'Content-type: application/json' --data @/tmp/slack.data.txt \
-		https://hooks.slack.com/services/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 \
-          &&  echo slack message sent ||  echo error sending slack msg
+	      https://hooks.slack.com/services/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 \
+       &&  echo slack message sent ||  echo error sending slack msg
 
